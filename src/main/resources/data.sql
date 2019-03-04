0e7b67088824413f81c7654b90e5afa24b6ee39c
shuto.masashi <shuto.masashi@tis.co.jp>
2019-03-04 18:01:51 +0900

    3/4日作業をコミット

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,48 @@
 INSERT INTO HELLO ( ID, MESSAGE ) VALUES ( 0, 'HELLO WORLD!' );
+
+INSERT INTO EMPLOYEE_ALBUM (
+    EMPLOYEE_ID
+    , FAMILY_NAME
+    , PERSONAL_NAME
+    , YEARLY
+    , BLOOD_TYPE
+    , TEAM
+    , CUSTOMER
+    , PROJECT
+    , PRIVATE_SENTENCE
+    , COMMENT
+)
+VALUES (
+    0
+    , 'にゃん'
+    , 'クー'
+    , 2
+    , 0
+    , '長毛'
+    , '人間'
+    , 'おなか見せ'
+    , 'ずっと寝てます。\r\nTulleが好きです。\r\nお風呂は嫌いです。'
+    , 'にゃーお'
+);
+
+INSERT INTO HISTORY (
+    HISTORY_ID
+    , EMPLOYEE_ID
+    , YEAR
+    , MONTH
+    , EVENT
+)
+VALUES (
+    0
+    , 0
+    , 2016
+    , 4
+    , '産まれる'
+)
+,(
+    1
+    , 0
+    , 2018
+    , 6
+    , '飼われる'
+);
