d80183c01ba6600ef68d957ad6b66ce616c11d73
unknown <tie302973@tisnt.tis.co.jp>
2020-02-19 19:26:49 +0900

    アカウント作成画面の登録ボタン遷移

--- src/main/java/jp/co/tis/climate/albumweb/presentation/controller/AccountController.java
+++ src/main/java/jp/co/tis/climate/albumweb/presentation/controller/AccountController.java
@@ -3,15 +3,21 @@ package jp.co.tis.climate.albumweb.presentation.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("account")
 public class AccountController {
 
-    @GetMapping
-    @RequestMapping("new")
-    public String accountNew(Model model) {
+    @GetMapping("/new")
+    public String display(Model model) {
         return "account/new";
     }
+
+    @PostMapping("/new")
+    public String registration(Model model) {
+        return "top";
+    }
+
 }
