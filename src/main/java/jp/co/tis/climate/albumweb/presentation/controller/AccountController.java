4448a85da608850a84490c09c438c6073a1c3d3e
unknown <tie302973@tisnt.tis.co.jp>
2020-02-25 19:14:19 +0900

    アカウント作成画面選択肢作成

--- src/main/java/jp/co/tis/climate/albumweb/presentation/controller/AccountController.java
+++ src/main/java/jp/co/tis/climate/albumweb/presentation/controller/AccountController.java
@@ -5,14 +5,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import jp.co.tis.climate.albumweb.domain.code.BloodType;
+import jp.co.tis.climate.albumweb.domain.code.City;
+import jp.co.tis.climate.albumweb.domain.code.Gender;
+import jp.co.tis.climate.albumweb.domain.code.Recruit;
 
 @Controller
 @RequestMapping("account")
 public class AccountController {
 
     @GetMapping("/new")
-    public String display(Model model) {
-        return "account/new";
+    public ModelAndView display() {
+    	ModelAndView model = new ModelAndView();
+    	model.addObject("bloodTypeList", BloodType.values());
+    	model.addObject("cityList", City.values());
+    	model.addObject("recruitList", Recruit.values());
+    	model.addObject("genderList", Gender.values());
+    	model.setViewName("account/new");
+        return model;
     }
 
     @PostMapping("/new")
