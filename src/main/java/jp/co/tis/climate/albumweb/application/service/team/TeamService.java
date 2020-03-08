0bf68fe55b2ad7fcafe6ada882d1eea033278718
ShioriWatanabe <bergen4698@gmail.com>
2020-03-08 18:24:51 +0900

    [add]teamserviseを追加。

--- src/main/java/jp/co/tis/climate/albumweb/application/service/team/TeamService.java
+++ src/main/java/jp/co/tis/climate/albumweb/application/service/team/TeamService.java
@@ -1,32 +1,24 @@
 package jp.co.tis.climate.albumweb.application.service.team;
 
-import static java.util.stream.Collectors.toList;
+import jp.co.tis.climate.albumweb.domain.model.member.Team;
+import jp.co.tis.climate.albumweb.infrastructure.dao.TeamDao;
+import jp.co.tis.climate.albumweb.presentation.validation.Month;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-
-import jp.co.tis.climate.albumweb.infrastructure.dao.TeamDao;
-import jp.co.tis.climate.albumweb.infrastructure.dao.UserDao;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 
 @Service
 public class TeamService {
 
     private final TeamDao teamDao;
 
-    public TeamService(TeamService teamService){ this.teamDao = teamDao;}
+    public TeamService(TeamDao teamDao){this.teamDao = teamDao;}
 
-    /**
-     *
-     * @param
-     * @param
-     * @return
-     */
-
+    @Transactional(propagation = Propagation.REQUIRED, readOnly = true)
+    public List<Team> findTeamList(){
+        return teamDao.findTeamList();
+    }
 }
 
